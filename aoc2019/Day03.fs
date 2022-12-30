154683a53629bd7268e588c585f8560e06a93e7a
Kevin Mees <kev.mees@gmail.com>
2022-12-30 15:33:01 +0100

    day 3: done

--- aoc2019/Day03.fs
+++ aoc2019/Day03.fs
@@ -8,14 +8,79 @@ open System
 open Xunit
 open AoC.Common.Prelude
 
-let p1 (input: String[]) = ""
-let p2 (input: String[]) = ""
+let parseLines =
+  split ","
+  >> Seq.map (splitAt 1)
+  >> Seq.map (fun (dir, len) -> parseDirection dir, int len)
 
+let follow (x, y) (dir: Direction) (len: int) =
+  let (dx, dy) = dxy dir
+
+  seq {
+    for i in 1..len do
+      yield (x + dx * i, y + dy * i, i)
+  }
+
+let createWire (lines: (Direction * int) seq) =
+  let rec createWire' (lines: (Direction * int) seq) (x, y, l) (acc: Map<Point, int>) =
+    match Seq.tryHead lines with
+    | Some(dir, len) ->
+      let points =
+        follow (x, y) dir len
+        |> Seq.map (fun (px, py, pl) -> (px, py, pl + l))
+        |> Seq.toArray
+
+      let acc = points |> Seq.fold (fun acc (px, py, pl) -> Map.add (px, py) pl acc) acc
+
+      let next = points |> Array.last
+
+      createWire' (lines |> Seq.skip 1) next acc
+    | None -> acc
+
+  createWire' lines (0, 0, 0) Map.empty
+
+let p1 (input: String[]) =
+  input
+  |> Seq.map parseLines
+  |> Seq.map createWire
+  |> Seq.map (Map.keys >> Set.ofSeq)
+  |> Set.intersectMany
+  |> Seq.map (manhattan (0, 0))
+  |> Seq.min
+
+let p2 (input: String[]) =
+  let wires = input |> Seq.map parseLines |> Seq.map createWire
+  let intersections = wires |> Seq.map (Map.keys >> Set.ofSeq) |> Set.intersectMany
+
+  intersections
+  |> Seq.map (fun p -> wires |> Seq.map (Map.find p) |> Seq.sum)
+  |> Seq.min
 
 // Tests
 
-let p1ex () : obj[] list = [ [| @""; 0 |] ]
-let p2ex () : obj[] list = [ [| @""; 0 |] ]
+let p1ex () : obj[] list =
+  [ [| @"
+        R8,U5,L5,D3
+        U7,R6,D4,L4
+       "
+       6 |]
+    [| @"
+        R75,D30,R83,U83,L12,D49,R71,U7,L72
+        U62,R66,U55,R34,D71,R55,D58,R83
+       "
+       159 |] ]
+
+let p2ex () : obj[] list =
+  [ [| @"
+        R8,U5,L5,D3
+        U7,R6,D4,L4
+       "
+       30 |]
+    [| @"
+        R75,D30,R83,U83,L12,D49,R71,U7,L72
+        U62,R66,U55,R34,D71,R55,D58,R83
+       "
+       610 |] ]
 
 [<Theory>]
 [<MemberData(nameof p1ex)>]
--- common/Prelude.fs
+++ common/Prelude.fs
@@ -7,7 +7,39 @@ module Prelude =
   let split (sep: string) (str: string) =
     str.Split([| sep |], StringSplitOptions.RemoveEmptyEntries)
 
+  let splitAt (index: int) (str: string) =
+    str.Substring(0, index), str.Substring(index)
+
   let splitlines (input: String) =
     input.Split("\n")
     |> Array.map (fun (x: String) -> x.Trim())
     |> Array.filter (fun (x: String) -> x <> "")
+
+  let manhattan (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+
+  type Direction =
+    | N
+    | E
+    | S
+    | W
+
+  type Point = int * int
+
+  let parseDirection (dir: string) =
+    match dir.ToUpper() with
+    | "N"
+    | "U" -> N
+    | "E"
+    | "R" -> E
+    | "S"
+    | "D" -> S
+    | "W"
+    | "L" -> W
+    | _ -> failwith "invalid direction"
+
+  let dxy (dir: Direction) =
+    match dir with
+    | N -> (0, 1)
+    | E -> (1, 0)
+    | S -> (0, -1)
+    | W -> (-1, 0)
