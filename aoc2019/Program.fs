026eebab46fa2acd90e4b16a6107ebb26b5a7e85
Kevin Mees <kev.mees@gmail.com>
2023-11-23 15:02:01 +0100

    feat: cli options support

--- aoc2019/Program.fs
+++ aoc2019/Program.fs
@@ -1,10 +1,24 @@
 module Program =
   open System.Reflection
 
+  let (|Int|_|) str =
+    match System.Int32.TryParse(str: string) with
+    | (true, int) -> Some(int)
+    | _ -> None
+
   [<EntryPoint>]
-  let main _ =
+  let main args =
     printfn "Advent of Code 2019"
     printfn "----------------------------------------------------------------"
-    AoC.Common.Runner.runLatest ()
+    printfn "Arguments passed to function : %A" args
+
+    match args[0] with
+    | Int i -> AoC.Common.Runner.runDay i
+    | "all"
+    | "a" -> AoC.Common.Runner.runAll ()
+    | "latest"
+    | "l"
+    | _ -> AoC.Common.Runner.runLatest ()
+
     printfn "\n----------------------------------------------------------------"
     0
